f36a153628e3237b096440639892d2e6db71b8b8
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-01-04 11:42:01 -0300

    feat: added logic to return screen

--- lib/components/page_basic.dart
+++ lib/components/page_basic.dart
@@ -1,4 +1,3 @@
-
 import 'package:car_fix/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,25 +5,46 @@ import 'package:get/get.dart';
 class PageBasic extends StatelessWidget {
   final Widget body;
   final bool canGoBack;
-  const PageBasic({super.key, required this.body, required this.canGoBack});
+  final String? parametersReturnScreen;
+  const PageBasic(
+      {super.key,
+      required this.body,
+      required this.canGoBack,
+      this.parametersReturnScreen});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: canGoBack ? IconButton(
-          onPressed: () {
-            Get.back();
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: ColorsProject.buttonPrimary,
-          ),
-        ) : null,
+        leading: canGoBack
+            ? IconButton(
+                onPressed: () {
+                  if (parametersReturnScreen != null) {
+                    if (parametersReturnScreen == "client") {
+                      Get.toNamed('/login/client');
+                    } else {
+                      Get.toNamed('/login/service_provider');
+                    }
+                  } else {
+                    Get.back();
+                  }
+                },
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  color: ColorsProject.buttonPrimary,
+                ),
+              )
+            : null,
         backgroundColor: ColorsProject.blackPrimary,
       ),
       backgroundColor: ColorsProject.blackPrimary,
-      body:(body)
+      body: Theme(
+        data: ThemeData(
+            canvasColor: ColorsProject.blackPrimary,
+            colorScheme:
+                const ColorScheme.light(primary: ColorsProject.greenPrimary)),
+        child: body,
+      ),
     );
   }
 }
