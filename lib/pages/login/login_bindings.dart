a1690ab96f313467af47c243578c44e4e4b3512c
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-01-13 09:29:13 -0300

    feat: inicializando as classes de UserRepository,UserRepositoryImpl,UserService,UserServiceImpl e LoginController

--- lib/pages/login/login_bindings.dart
+++ lib/pages/login/login_bindings.dart
@@ -1,11 +1,29 @@
 import 'package:car_fix/pages/login/login_controller.dart';
+import 'package:car_fix/repository/user/user_repository.dart';
+import 'package:car_fix/repository/user/user_repository_impl.dart';
+import 'package:car_fix/service/user/user_service.dart';
+import 'package:car_fix/service/user/user_service_impl.dart';
 import 'package:get/get.dart';
 
 class LoginBindings implements Bindings {
   @override
   void dependencies() {
-    Get.put(
-      LoginController(),
+    Get.lazyPut<UserRepository>(
+      () => UserRepositoryImpl(),
+      fenix: true,
+    );
+
+    Get.lazyPut<UserService>(
+      () => UserServiceImpl(
+        userRepository: Get.find(),
+      ),
+      fenix: true,
+    );
+
+    Get.lazyPut(
+      () => LoginController(
+        userService: Get.find(),
+      ),
     );
   }
 }
