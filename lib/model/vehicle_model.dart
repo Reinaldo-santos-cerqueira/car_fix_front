5c84fbc825bbacecfcde0526dd2714ce16429b27
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-24 16:18:38 -0300

    feat: adicionando novo campo path_to_document

--- lib/model/vehicle_model.dart
+++ lib/model/vehicle_model.dart
@@ -1,24 +1,35 @@
 class VehicleModel {
-  String? plate;
-  String? color;
-  String? model;
-  String? mark;
+  final String model;
+  final String mark;
+  final String plate;
+  final String color;
+  final String pathToDocument;
 
-  VehicleModel({this.plate, this.color, this.model, this.mark});
+  VehicleModel({
+    required this.model,
+    required this.mark,
+    required this.plate,
+    required this.color,
+    required this.pathToDocument,
+  });
 
-  VehicleModel.fromJson(Map<String, dynamic> json) {
-    plate = json['plate'];
-    color = json['color'];
-    model = json['model'];
-    mark = json['mark'];
+  factory VehicleModel.fromJson(Map<String, dynamic> json) {
+    return VehicleModel(
+      model: json['model'],
+      mark: json['mark'],
+      plate: json['plate'],
+      color: json['color'],
+      pathToDocument: json['path_to_document'],
+    );
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['plate'] = plate;
-    data['color'] = color;
-    data['model'] = model;
-    data['mark'] = mark;
-    return data;
+    return {
+      'model': model,
+      'mark': mark,
+      'plate': plate,
+      'color': color,
+      'path_to_document': pathToDocument,
+    };
   }
 }
