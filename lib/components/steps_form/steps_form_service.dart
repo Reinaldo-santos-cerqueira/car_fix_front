03182abb50589d7f5bbc6d5f060889e76b22e0d3
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-24 16:29:38 -0300

    refactor: removendo valor de km e serviço e envolvendo o elemento em obx para atualização da listagem dos serviços

--- lib/components/steps_form/steps_form_service.dart
+++ lib/components/steps_form/steps_form_service.dart
@@ -16,93 +16,56 @@ class _StepsFormServiceState extends State<StepsFormService> {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<SignUpServiceProviderController>();
-    return ListView.builder(
-      itemCount: controller.listService.length,
-      shrinkWrap: true,
-      itemBuilder: (context, index) {
-        ServiceModel service = controller.listService[index];
-        return Form(
-          key: controller.listService[index].formKey,
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    CheckboxListTile(
-                      side: BorderSide.none,
-                      fillColor: WidgetStateProperty.all<Color>(
-                        ColorsProject.greenPrimary,
-                      ),
-                      title: Text(
-                        service.title,
-                        style: const TextStyle(
-                          color: ColorsProject.gray4,
-                          fontSize: 18,
-                        ),
-                      ),
-                      subtitle: Text(
-                        service.description,
-                        style: const TextStyle(
-                          color: ColorsProject.gray3,
-                          fontSize: 16,
-                        ),
-                      ),
-                      value: service.isSelected,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          service.isSelected = value ?? false;
-                        });
-                      },
-                    ),
-                    AnimatedCrossFade(
-                      firstChild: Container(height: 0,),
-                      secondChild: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const SizedBox(height: 20,),
-                          TextFormWithoutIconFieldCustom(
-                            controller:
-                                service.textEditingControllerPriceService!,
-                            hintText: 'Digite o preço do serviço',
-                            textInputType: TextInputType.number,
-                            validator: (value) {
-                              if (service.isSelected &&
-                                  (value == null || value.isEmpty)) {
-                                return 'Campo obrigatório';
-                              }
-                              return null;
-                            },
+    return Obx(
+      () => (
+        ListView.builder(
+          itemCount: controller.listService.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            ServiceModel service = controller.listService[index];
+            return Form(
+              key: controller.listService[index].formKey,
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    child: Column(
+                      children: [
+                        CheckboxListTile(
+                          side: BorderSide.none,
+                          fillColor: WidgetStateProperty.all<Color>(
+                            ColorsProject.greenPrimary,
                           ),
-                          const SizedBox(height: 20),
-                          TextFormWithoutIconFieldCustom(
-                            textInputType: TextInputType.number,
-                            controller:
-                                service.textEditingControllerPriceTraveled!,
-                            hintText: 'Digite o valor cobrado por km',
-                            validator: (value) {
-                              if (service.isSelected &&
-                                  (value == null || value.isEmpty)) {
-                                return 'Campo obrigatório';
-                              }
-                              return null;
-                            },
+                          title: Text(
+                            service.title,
+                            style: const TextStyle(
+                              color: ColorsProject.gray4,
+                              fontSize: 18,
+                            ),
                           ),
-                        ],
-                      ),
-                      crossFadeState: service.isSelected
-                          ? CrossFadeState.showSecond
-                          : CrossFadeState.showFirst,
-                      duration: const Duration(milliseconds: 700),
+                          subtitle: Text(
+                            service.description,
+                            style: const TextStyle(
+                              color: ColorsProject.gray3,
+                              fontSize: 16,
+                            ),
+                          ),
+                          value: service.isSelected,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              service.isSelected = value ?? false;
+                            });
+                          },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        );
-      },
+            );
+          },
+        ),
+      ),
     );
   }
 }
