b6560bae75a45c078876fd0bdbe7235f76a7a737
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-24 16:34:17 -0300

    fix: corrigindo erro causado pela ,

--- lib/components/steps_form/steps_form_service.dart
+++ lib/components/steps_form/steps_form_service.dart
@@ -16,56 +16,51 @@ class _StepsFormServiceState extends State<StepsFormService> {
   @override
   Widget build(BuildContext context) {
     final controller = Get.find<SignUpServiceProviderController>();
-    return Obx(
-      () => (
-        ListView.builder(
-          itemCount: controller.listService.length,
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            ServiceModel service = controller.listService[index];
-            return Form(
-              key: controller.listService[index].formKey,
-              child: Column(
-                children: [
-                  Container(
-                    width: double.infinity,
-                    child: Column(
-                      children: [
-                        CheckboxListTile(
-                          side: BorderSide.none,
-                          fillColor: WidgetStateProperty.all<Color>(
-                            ColorsProject.greenPrimary,
-                          ),
-                          title: Text(
-                            service.title,
-                            style: const TextStyle(
-                              color: ColorsProject.gray4,
-                              fontSize: 18,
-                            ),
-                          ),
-                          subtitle: Text(
-                            service.description,
-                            style: const TextStyle(
-                              color: ColorsProject.gray3,
-                              fontSize: 16,
-                            ),
-                          ),
-                          value: service.isSelected,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              service.isSelected = value ?? false;
-                            });
-                          },
+    return Obx(() => (ListView.builder(
+        itemCount: controller.listService.length,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          ServiceModel service = controller.listService[index];
+          return Form(
+            key: controller.listService[index].formKey,
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      CheckboxListTile(
+                        side: BorderSide.none,
+                        fillColor: WidgetStateProperty.all<Color>(
+                          ColorsProject.greenPrimary,
                         ),
-                      ],
-                    ),
+                        title: Text(
+                          service.title,
+                          style: const TextStyle(
+                            color: ColorsProject.gray4,
+                            fontSize: 18,
+                          ),
+                        ),
+                        subtitle: Text(
+                          service.description,
+                          style: const TextStyle(
+                            color: ColorsProject.gray3,
+                            fontSize: 16,
+                          ),
+                        ),
+                        value: service.isSelected,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            service.isSelected = value ?? false;
+                          });
+                        },
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
-    );
+                ),
+              ],
+            ),
+          );
+        })));
   }
 }
