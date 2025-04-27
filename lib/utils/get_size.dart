a4b90c006ec9e6afe882038c7ca8e4cd9eb68f81
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-27 00:00:19 -0300

    feat: adicionando duas funções para trazer o tamnanho da altura e largura pela porcentagem

--- lib/utils/get_size.dart
+++ lib/utils/get_size.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class GetSize {
-  Size sizeScreen(context) {
+  Size sizeScreen(BuildContext context) {
     return MediaQuery.of(context).size;
   }
 
-  double getHeightWithoutAppBar(context){
+  double getHeightWithoutAppBar(BuildContext context){
     double screenHeight = MediaQuery.of(context).size.height;
     double appBarHeight = AppBar().preferredSize.height;
     double statusBarHeight = MediaQuery.of(context).padding.top;
     return screenHeight - appBarHeight - statusBarHeight - 20;
   }
+
+  double getHeightFromPercent(BuildContext context,double percent){
+    return sizeScreen(context).height / 100 * percent;
+  }
+  double getWidthFromPercent(BuildContext context,double percent){
+    return sizeScreen(context).width / 100 * percent;
+  }
+
 }
