7f011960889763c20058896df94957a938ab0df1
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-28 23:55:24 -0300

    feat: mudando a forma de pegar os valores

--- lib/model/service_model.dart
+++ lib/model/service_model.dart
@@ -1,33 +1,27 @@
-import 'package:flutter/widgets.dart';
-
 class ServiceModel {
   final String id;
   final String description;
   final String title;
-  final TextEditingController? textEditingControllerPriceService;
-  final TextEditingController? textEditingControllerPriceTraveled;
+  final double priceService;
+  final double priceKmTraveled;
   bool isSelected;
-  GlobalKey<FormState>? formKey;
 
   ServiceModel({
     required this.id,
     required this.description,
     required this.title,
-    TextEditingController? textEditingControllerPriceService,
-    TextEditingController? textEditingControllerPriceTraveled,
+    required this.priceService,
+    required this.priceKmTraveled,
     this.isSelected = false,
-    GlobalKey<FormState>? formKey,
-  })  : formKey = formKey ?? GlobalKey<FormState>(),
-        textEditingControllerPriceService =
-            textEditingControllerPriceService ?? TextEditingController(),
-        textEditingControllerPriceTraveled =
-            textEditingControllerPriceTraveled ?? TextEditingController();
+  });
 
   factory ServiceModel.fromJson(Map<String, dynamic> json) {
     return ServiceModel(
       id: json['id'] as String,
       description: json['description'] as String,
       title: json['title'] as String,
+      priceService: (json['price_service'] as num).toDouble(),
+      priceKmTraveled: (json['price_km_traveled'] as num).toDouble(),
     );
   }
 
@@ -36,51 +30,8 @@ class ServiceModel {
       'id': id,
       'description': description,
       'title': title,
-      'priceService': double.parse(textEditingControllerPriceService!.text),
-      'priceKmTraveled': double.parse(textEditingControllerPriceTraveled!.text),
+      'price_service': priceService,
+      'price_km_traveled': priceKmTraveled,
     };
   }
-
-  ServiceModelApiServiceProvider getServiceSendApi() {
-    return ServiceModelApiServiceProvider(
-      serviceId: id,
-      priceService: double.parse(
-          textEditingControllerPriceService!.text.replaceAll(",", ".")),
-      priceKmTraveled: double.parse(
-          textEditingControllerPriceTraveled!.text.replaceAll(",", ".")),
-    );
-  }
-}
-
-class ServiceModelApiServiceProvider {
-  final String serviceId;
-  final double priceService;
-  final double priceKmTraveled;
-
-  ServiceModelApiServiceProvider({
-    required this.serviceId,
-    required this.priceService,
-    required this.priceKmTraveled,
-  });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'serviceId': serviceId,
-      'priceService': priceService.toStringAsFixed(2),
-      'priceKmTraveled': priceKmTraveled.toStringAsFixed(2),
-    };
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    if (other is! ServiceModelApiServiceProvider) return false;
-    return serviceId == other.serviceId &&
-        priceService == other.priceService &&
-        priceKmTraveled == other.priceKmTraveled;
-  }
-
-  @override
-  int get hashCode =>
-      serviceId.hashCode ^ priceService.hashCode ^ priceKmTraveled.hashCode;
 }
