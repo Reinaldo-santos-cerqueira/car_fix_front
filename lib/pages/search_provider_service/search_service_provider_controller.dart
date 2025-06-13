2b7bfd0748e3ae1e42eba462fadb9fbfe522e2f5
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-06-13 17:57:20 -0300

    feat: finalizando tela de search_service_provider

--- lib/pages/search_provider_service/search_service_provider_controller.dart
+++ lib/pages/search_provider_service/search_service_provider_controller.dart
@@ -1,5 +1,103 @@
+import 'dart:async';
+
+import 'package:car_fix/model/confirm_request_arguments_model.dart';
+import 'package:car_fix/model/service_requested_accept_provider.dart';
+import 'package:car_fix/utils/url.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SearchServiceProviderController extends GetxController {
+  final Rx<String?> userId = "".obs;
+  final Rx<String?> serviceRequestedId = "".obs;
+  late IO.Socket socket;
+  final ConfirmRequestArgumentsModel args =
+      Get.arguments as ConfirmRequestArgumentsModel;
+  final RxList<ServiceRequestedAcceptProviderModel>
+      listServiceRequestedAcceptProviderModel =
+      <ServiceRequestedAcceptProviderModel>[].obs;
+  late Timer timer;
+  final Rx<ServiceRequestedAcceptProviderModel>
+      selectedServiceRequestedAcceptProviderModel =
+      ServiceRequestedAcceptProviderModel.empty().obs;
+  @override
+  void onInit() async {
+    super.onInit();
+    final prefs = await SharedPreferences.getInstance();
+    userId.value = prefs.getString('userId');
+    socket = IO.io(urlMain, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+    socket.connect();
 
+    socket.onConnect((_) {
+      if (listServiceRequestedAcceptProviderModel.isEmpty) {
+        requestService();
+      }
+    });
+
+    socket.on("accepted_service", (msg) {
+      ServiceRequestedAcceptProviderModel serviceRequestedAcceptProvider =
+          ServiceRequestedAcceptProviderModel.fromMap(msg);
+      listServiceRequestedAcceptProviderModel
+          .add(serviceRequestedAcceptProvider);
+      print(msg);
+    });
+
+    socket.on("received_service", (msg) {
+      serviceRequestedId.value = msg["id"];
+    });
+
+    socket.on("confirmed_start_service", (msg) {
+      selectedServiceRequestedAcceptProviderModel.value.id =
+          serviceRequestedId.value;
+      Get.offAllNamed("/waiting_service_provider",
+          arguments: selectedServiceRequestedAcceptProviderModel.value);
+    });
+
+    timer = Timer.periodic(const Duration(minutes: 2), (timer) {
+      if (listServiceRequestedAcceptProviderModel.isEmpty) {
+        requestService();
+      }
+    });
+  }
+
+  void requestService() {
+    if (serviceRequestedId.value == "") {
+      socket.emit('request_service', {
+        "service_id": args.serviceId,
+        "user_id_client": userId.value,
+        "latitude_client": args.latitude,
+        "longitude_client": args.longitude,
+        "status": 0,
+        "vehicle_id_client": args.vehicle!.id
+      });
+    } else {
+      socket.emit('request_service', {
+        "service_id": args.serviceId,
+        "user_id_client": userId.value,
+        "latitude_client": args.latitude,
+        "longitude_client": args.longitude,
+        "status": 0,
+        "vehicle_id_client": args.vehicle!.id,
+        "id": serviceRequestedId.value
+      });
+    }
+  }
+
+  void acceptRequestClient(
+      ServiceRequestedAcceptProviderModel
+          selectedServiceRequestedAcceptProvider) {
+    selectedServiceRequestedAcceptProviderModel.value =
+        selectedServiceRequestedAcceptProvider;
+    socket.emit('accept_service_client',
+        {"serviceRequestedId": serviceRequestedId.value, "status": 2});
+  }
+
+  @override
+  void onClose() {
+    // socket.dispose();
+    super.onClose();
+  }
 }
--- lib/pages/search_provider_service/search_service_provider_screen.dart
+++ lib/pages/search_provider_service/search_service_provider_screen.dart
@@ -1,14 +1,276 @@
+import 'package:car_fix/components/app_bar.dart';
+import 'package:car_fix/components/buttons/primary_button.dart';
 import 'package:car_fix/pages/search_provider_service/search_service_provider_controller.dart';
+import 'package:car_fix/utils/url.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class SearchServiceProviderScreen extends GetView<SearchServiceProviderController> {
+import '../../utils/colors.dart';
+
+class SearchServiceProviderScreen
+    extends GetView<SearchServiceProviderController> {
   const SearchServiceProviderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:  Text("SearchServiceProvider"),
+    return Scaffold(
+      appBar: const AppBarCustom(title: ""),
+      backgroundColor: ColorsProject.blackPrimary,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Obx(
+          () {
+            return controller.listServiceRequestedAcceptProviderModel.isEmpty
+                ? const Center(
+                    child: Text("Procurando motoristas"),
+                  )
+                : ListView.separated(
+                    itemCount: controller
+                        .listServiceRequestedAcceptProviderModel.length,
+                    itemBuilder: (context, index) {
+                      var providerService = controller
+                          .listServiceRequestedAcceptProviderModel[index];
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 30,
+                                    foregroundImage: NetworkImage(
+                                        "$urlMain/${providerService.pathProfileImage}"),
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                      providerService.fullName,
+                                      style: const TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold,
+                                        color: ColorsProject.white1,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Modelo: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: providerService.vehicles[0].model,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Marca: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: providerService.vehicles[0].mark,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Placa: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: providerService.vehicles[0].plate,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Color: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: providerService.vehicles[0].color,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Distância: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "${providerService.distance} Km",
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Duração: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text:
+                                          "${providerService.duration} minutos",
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              RichText(
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                text: TextSpan(
+                                  children: <TextSpan>[
+                                    const TextSpan(
+                                      text: 'Valor total: ',
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text:
+                                          "R\$ ${providerService.valueService.toStringAsFixed(2)}",
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Color(0xFFE1E1E1),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              PrimaryButton(
+                                  onPressed: () {
+                                    controller
+                                        .acceptRequestClient(providerService);
+                                  },
+                                  text: "Confirmar pedido")
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        height: 10,
+                      );
+                    },
+                  );
+          },
+        ),
+      ),
     );
   }
 }
