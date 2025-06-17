0495d7c51ddb8ed84b26ba620876a088e6fa2dcc
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-06-16 22:12:04 -0300

    feat: mudando um pouco o layout

--- lib/pages/login/login_screen.dart
+++ lib/pages/login/login_screen.dart
@@ -15,116 +15,116 @@ class LoginScreen extends GetView<LoginController> {
 
   @override
   Widget build(BuildContext context) {
+    final isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
+
     return PageBasic(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SafeArea(
-          child: Form(
-            key: controller.formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Container(),
-                ),
-                TextFormFieldCustom(
-                  controller: controller.textEditingControllerEmail,
-                  hintText: "Digite seu email",
-                  icon: Icons.mail,
-                  validator: Validatorless.multiple([
-                    Validatorless.email("Digite um email valido"),
-                    Validatorless.required("Email é requirido'")
-                  ]),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormFieldPasswordCustom(
-                  controller: controller.textEditingControllerPassword,
-                  hintText: "Digite sua senha",
-                  icon: Icons.lock,
-                  validator: Validatorless.regex(
-                    RegExp(
-                        r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$'),
-                    "Digite uma senha válida",
+      canGoBack: true,
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            physics: isKeyboardOpen
+                ? const AlwaysScrollableScrollPhysics()
+                : const NeverScrollableScrollPhysics(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SafeArea(
+                  maintainBottomViewPadding: true,
+                  child: Form(
+                    key: controller.formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextFormFieldCustom(
+                          controller: controller.textEditingControllerEmail,
+                          hintText: "Digite seu email",
+                          icon: Icons.mail,
+                          textInputType: TextInputType.emailAddress,
+                          validator: Validatorless.multiple([
+                            Validatorless.email("Digite um email valido"),
+                            Validatorless.required("Email é requirido'")
+                          ]),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormFieldPasswordCustom(
+                          controller: controller.textEditingControllerPassword,
+                          hintText: "Digite sua senha",
+                          icon: Icons.lock,
+                          validator: Validatorless.regex(
+                            RegExp(
+                                r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$'),
+                            "Digite uma senha válida",
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            const Text(
+                              'Problemas para fazer login? ',
+                              style:
+                                  TextStyle(color: ColorsProject.buttonPrimary),
+                            ),
+                            TextButtonCustom(
+                              onPressed: () {
+                                Get.toNamed("/trade_password");
+                              },
+                              text: "Clique aqui",
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        PrimaryButton(
+                          onPressed: () {
+                            controller.login();
+                          },
+                          text: 'Login',
+                        ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 1,
+                                color: ColorsProject.buttonPrimary,
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            const Text(
+                              "OU",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: ColorsProject.buttonPrimary,
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: Container(
+                                height: 1,
+                                color: ColorsProject.buttonPrimary,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        OutlinedButtonCustom(
+                          onPressed: () {
+                            controller.goSignUp();
+                          },
+                          text: 'Cadastrar-se',
+                        ),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    const Text(
-                      'Problemas para fazer login? ',
-                      style: TextStyle(color: ColorsProject.buttonPrimary),
-                    ),
-                    TextButtonCustom(
-                      onPressed: () {
-                        Get.toNamed("/trade_password");
-                      },
-                      text: "Clique aqui",
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                PrimaryButton(
-                  onPressed: () {
-                    controller.login();
-                  },
-                  text: 'Login',
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        height: 1,
-                        color: ColorsProject.buttonPrimary,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    const Text(
-                      "OU",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: ColorsProject.buttonPrimary),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Expanded(
-                      child: Container(
-                        height: 1,
-                        color: ColorsProject.buttonPrimary,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                OutlinedButtonCustom(
-                  onPressed: () {
-                    controller.goSignUp();
-                  },
-                  text: 'Cadastrar-se',
-                ),
-                const SizedBox(
-                  height: 30,
-                )
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
-      canGoBack: true,
     );
   }
 }
