9823970e4e9c2bd7c0d1badf1e0b06cbe9b4370f
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-07-10 19:41:25 -0300

    feat: atualizando a tela de prestador de servidor home

--- lib/pages/home_service_provider/home_service_provider_controller.dart
+++ lib/pages/home_service_provider/home_service_provider_controller.dart
@@ -1,6 +1,81 @@
+import 'dart:async';
+
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:latlong2/latlong.dart';
 
 class HomeServiceProviderController extends GetxController {
+  Rx<LatLng> currentPosition = const LatLng(0, 0).obs;
+  late MapController mapController = MapController();
+  Rx<bool> loading = false.obs;
+  Rx<String> street = "".obs;
+  Rx<String> neighborhood = "".obs;
+  final findPosition = false.obs;
+  StreamSubscription<Position>? _positionStream;
 
+  @override
+  void onInit() async {
+    super.onInit();
+    if (await checkLocationPermission()) {
+      getPosition();
+    }
+  }
+
+  void getPosition() async {
+    try {
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        print("Serviço de localização desativado.");
+        return;
+      }
+
+      const LocationSettings locationSettings = LocationSettings(
+        accuracy: LocationAccuracy.bestForNavigation,
+        distanceFilter: 10,
+      );
+
+      _positionStream =
+          Geolocator.getPositionStream(locationSettings: locationSettings)
+              .listen((Position? position) async {
+        if (position == null) return;
+
+        currentPosition.value = LatLng(position.latitude, position.longitude);
+        mapController.move(currentPosition.value, 17.0);
+
+        List<Placemark> placemarks = await placemarkFromCoordinates(
+            position.latitude, position.longitude);
+        if (placemarks.isNotEmpty) {
+          Placemark placemark = placemarks.first;
+          street.value = placemark.street ?? "Erro ao encontrar o local";
+          neighborhood.value = placemark.subLocality ?? "";
+        }
+      });
+    } catch (e) {
+      print("Erro ao obter o placemark: $e");
+    } finally {
+      loading(false);
+    }
+  }
+
+  Future<bool> checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      LocationPermission requestedPermission =
+          await Geolocator.requestPermission();
+      if (requestedPermission != LocationPermission.whileInUse &&
+          requestedPermission != LocationPermission.always) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @override
+  void onClose() {
+    _positionStream?.cancel();
+    super.onClose();
+  }
 }
--- lib/pages/home_service_provider/home_service_provider_screen.dart
+++ lib/pages/home_service_provider/home_service_provider_screen.dart
@@ -1,14 +1,132 @@
 import 'package:car_fix/pages/home_service_provider/home_service_provider_controller.dart';
+import 'package:car_fix/utils/colors.dart';
+import 'package:car_fix/utils/get_size.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomeServiceProviderScreen extends GetView<HomeServiceProviderController> {
   const HomeServiceProviderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:  Text("Home service provider"),
+    var getSize = GetSize();
+    return Obx(
+      () {
+        return Scaffold(
+          bottomNavigationBar: const GNav(
+            backgroundColor: Colors.black,
+            color: ColorsProject.white1,
+            activeColor: ColorsProject.white1,
+            gap: 10,
+            tabMargin: EdgeInsets.symmetric(horizontal: 30),
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: "Inicio",
+              ),
+              GButton(
+                icon: Icons.list,
+                text: "Historico",
+              ),
+            ],
+          ),
+          body: controller.loading.value
+              ? const Center(
+                  child: Text("Carregando..."),
+                )
+              : Stack(
+                  children: [
+                    FlutterMap(
+                      mapController: controller.mapController,
+                      options: MapOptions(
+                        initialCenter: controller.currentPosition.value,
+                        initialZoom: 17.0,
+                        interactionOptions: const InteractionOptions(
+                          flags: InteractiveFlag.none,
+                        ),
+                      ),
+                      children: [
+                        TileLayer(
+                          urlTemplate:
+                              "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          subdomains: const ['a', 'b', 'c'],
+                        ),
+                        MarkerLayer(
+                          markers: [
+                            Marker(
+                              width: 80.0,
+                              height: 80.0,
+                              point: controller.currentPosition.value,
+                              child: const Icon(
+                                Icons.location_on,
+                                color: Colors.red,
+                                size: 40.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          decoration: const BoxDecoration(
+                            color: Color(0x88000000),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 20, left: 20, top: 100),
+                      child: Container(
+                        width: double.infinity,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.black,
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black38,
+                              offset: Offset(5, 5),
+                              blurRadius: 10,
+                              spreadRadius: 2,
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                          child: Row(
+                            children: [
+                              const Icon(
+                                Icons.location_on,
+                                color: ColorsProject.white1,
+                                size: 25.0,
+                              ),
+                              const SizedBox(width: 10),
+                              Expanded(
+                                child: Text(
+                                  "${controller.street.value}, ${controller.neighborhood.value}",
+                                  style: const TextStyle(
+                                      fontSize: 16,
+                                      color: ColorsProject.white1),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child:
+                          Container(color: Colors.amber, width: 50, height: 50),
+                    )
+                  ],
+                ),
+        );
+      },
     );
   }
 }
